c3e1f0f46a0c87a992fea84b3d063e797f5a18b8
EmeraldNZ <ahyuancy@gmail.com>
2018-12-23 12:27:33 +1300

    Update .js

--- .js
+++ .js
@@ -1,3 +1,29 @@
+/**************   Solution #1             ********************************************************************************************/
+
+function destroyer1(arr) {
+
+    let argumentsArr = Array.prototype.slice.call(arguments);
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < argumentsArr.length; j++){
+            if(arr[i] === argumentsArr[j]){
+                delete arr[i];
+            }
+        }
+    }
+    return arr.filter(Boolean); /* The Boolean object is an object wrapper for a boolean value.
+                                   If value parameter is omitted or is 0, -0, null, false, NaN, undefined, or the empty string (""), 
+                                      the object has an initial value of false    */
+
+}
+destroyer1([1, 2, 3, 1, 2, 3], 2, 3); // return [1, 1]
+destroyer1([0, 1, 2, 3, 1, 2, 3], 2, 3); // return [1, 1] -> ERROR! As 0 has a value of false thus it's also filtered 
+
+
+
+
+/**************   Solution #2  is the improved version of the above code    *************************************************/
+
 function destroyer(arr) {
   let newArr = [];
   for(let i = 1; i < arguments.length; i++){
@@ -11,13 +37,13 @@ function destroyer(arr) {
   }
   return updateArr;
 }
-destroyer([1, 2, 3, 1, 2, 3], 2, 3); // return [1, 1]
+destroyer2([1, 2, 3, 1, 2, 3], 2, 3); // return [1, 1]
 
-destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3); // return [1, 5, 1]
+destroyer2([1, 2, 3, 5, 1, 2, 3], 2, 3); // return [1, 5, 1]
 
-destroyer([3, 5, 1, 2, 2], 2, 3, 5); // return [1]
+destroyer2([3, 5, 1, 2, 2], 2, 3, 5); // return [1]
 
-destroyer([2, 3, 2, 3], 2, 3); // return []
+destroyer2([2, 3, 2, 3], 2, 3); // return []
 
 
 
