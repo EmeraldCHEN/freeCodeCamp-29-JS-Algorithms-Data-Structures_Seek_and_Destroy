2f56a481e1e836d6ac070fc3d2c0e13f829597df
EmeraldNZ <ahyuancy@gmail.com>
2018-12-22 20:10:46 +1300

    Update .js

--- .js
+++ .js
@@ -1,12 +1,9 @@
 function destroyer(arr) {
-
   let newArr = [];
-
   for(let i = 1; i < arguments.length; i++){
     newArr.push(arguments[i]);
   }      
   let updateArr =[];
-
   for(let j = 0; j <  arguments[0].length; j++){
     if(newArr.includes(arguments[0][j]) === false ){
       updateArr.push(arguments[0][j]);     
@@ -14,7 +11,6 @@ function destroyer(arr) {
   }
   return updateArr;
 }
-
 destroyer([1, 2, 3, 1, 2, 3], 2, 3); // return [1, 1]
 
 destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3); // return [1, 5, 1]
@@ -22,3 +18,11 @@ destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3); // return [1, 5, 1]
 destroyer([3, 5, 1, 2, 2], 2, 3, 5); // return [1]
 
 destroyer([2, 3, 2, 3], 2, 3); // return []
+
+
+
+
+
+// Retrieved from
+// https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/seek-and-destroy/
+
